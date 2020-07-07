7ffcfd27df0d3e344dc57098f9ffc7cced53756c
root <root@localhost.localdomain>
2020-07-07 09:28:55 +0800

    0707redis

--- onestep.sh
+++ onestep.sh
@@ -87,7 +87,7 @@ mv composer.phar /usr/bin/composer
 
 #install nodejs
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-sleep 3
+wait
 . ~/.nvm/nvm.sh
 nvm install node
 node -e "console.log('Running Node.js ' + process.version)"
